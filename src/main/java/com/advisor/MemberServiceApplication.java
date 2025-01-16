4b0d7cfb819f9ec4d0f7039728f9a57a1934b405
rlaehddud475 <6070kjh475@naver.com>
2025-01-16 17:51:18 +0900

    .
    # Conflicts:
    #	.gitignore
    #	build.gradle
    #	gradlew
    #	settings.gradle

--- src/main/java/com/advisor/MemberServiceApplication.java
+++ src/main/java/com/advisor/MemberServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MemverServiceApplication {
+public class MemberServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MemverServiceApplication.class, args);
+		SpringApplication.run(MemberServiceApplication.class, args);
 	}
 
 }
