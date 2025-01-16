598c9a1c90fef929af3e7c39561699fbb979f99b
rlaehddud475 <6070kjh475@naver.com>
2025-01-16 15:52:04 +0900

    초기 설정

---
+++ src/main/java/com/member/MemverServiceApplication.java
@@ -0,0 +1,13 @@
+package com.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemverServiceApplication.class, args);
+	}
+
+}
